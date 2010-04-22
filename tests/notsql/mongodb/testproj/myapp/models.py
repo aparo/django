ffb1f59441d3c55b06056090cf0a16ffe4673e54
Alberto Paro <alberto@ingparo.it>
2010-04-22 11:35:06 +0200

    Ordering of some fields to make them common to many nosql engines

--- tests/notsql/mongodb/testproj/myapp/models.py
+++ tests/notsql/mongodb/testproj/myapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.db.backends.mongodb.fields import ListField, SortedListField, DictField, SetListField, EmbeddedModel
+from django.db.backends.mongodb.fields import EmbeddedModel
+from django.db.models.nosql.fields import ListField, SortedListField, DictField, SetListField
 
 class StringForeignKey(models.ForeignKey):
 
